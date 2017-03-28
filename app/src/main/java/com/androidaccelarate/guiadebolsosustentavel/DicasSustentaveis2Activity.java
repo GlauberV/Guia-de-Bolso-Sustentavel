3a661b9692aa71df69cbec8b1b46b3d897df5c3d
GlauberV <glaubervinicius2@gmail.com>
2017-03-28 12:04:58 -0300

    Terminado a DicasSustentaveis2Activity

---
+++ app/src/main/java/com/androidaccelarate/guiadebolsosustentavel/DicasSustentaveis2Activity.java
@@ -0,0 +1,13 @@
+package com.androidaccelarate.guiadebolsosustentavel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DicasSustentaveis2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dicas_sustentaveis2);
+    }
+}
